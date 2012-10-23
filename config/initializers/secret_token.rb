556a420d14d189886f2e195d90bfe1d8647d409d
loredanagiurca <loredanagiurca@student.(none)>
2012-10-23 08:53:51 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ioanabianca::Application.config.secret_token = '93fff0da2ccfa3d7720ae2a66e7a1825e99ae55a74afe4351530a527bdfb6ce8416e8e36bf3a3a62a78b12b51df2071ae51bc0d9b42d37240171b8895081b5d3'
