bbeec3a6d19c0fb35508a43ad163b433a12ef033
loredanagiurca <loredanagiurca@student.(none)>
2012-10-23 10:58:21 +0300

    Commit 2

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :first_name, :last_name
+  has_one :cv , :foreign_key => 'user_id' 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ioanabianca::Application.routes.draw do
+  resources :cvs
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
