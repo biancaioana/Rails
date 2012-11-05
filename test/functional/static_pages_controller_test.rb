5a7412c14e2336270eca84a64a60067d95b0955e
Bianca <bianca@bianca-Lenovo-G550.(none)>
2012-11-06 01:05:40 +0200

    TestViews

--- test/functional/static_pages_controller_test.rb
+++ test/functional/static_pages_controller_test.rb
@@ -16,4 +16,30 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+#tests home page for a title
+
+test "should have a title" do
+    get :home
+    assert_select 'title', "Home"
+end
+
+
+#Presence of email field in new user view form
+
+test "should have email password fields" do
+  get :new
+  #select form
+  assert_select 'form' do
+    #select css element label from form and for each one do
+  css_select("label").each do |l|
+    #select css label with "Email" tag
+    name = css_select(l, "Email") 
+    #if exists respond "success"
+    if name
+       assert_response :success
+    end
+  end
+end
+end
+
 end
